7cadfcc9d2ed8d0c69b43137b2dca3f5a8cd5bd2
Daisuke Fujihara <nuklejp@yahoo.co.jp>
2019-06-26 04:03:49 +0000

    PHP13章

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -11,4 +11,11 @@ class NewsController extends Controller
     public function add(){
         return view('admin.news.create');
     }
+    
+    // 以下を追記
+  public function create(Request $request)
+  {
+      // admin/news/createにリダイレクトする
+      return redirect('admin/news/create');
+  }  
 }
